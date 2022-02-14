93c8c6b394cab22f06dc7c59f87a4f9d8a76cd6e
Houdou <houdou.pt@gmail.com>
2022-02-14 17:39:05 +0800

    Fix fs logger

--- GN.server/Utils/Logger.js
+++ GN.server/Utils/Logger.js
@@ -6,23 +6,23 @@ var Logger = function(options, data) {
     if(options.fileName != undefined) {
         this.fileName = options.fileName;
         this.absolutePath = rootPath + "/GNLog/" + options.fileName;
-        
+
         this.createTime = new Date();
         this.createTime.setTime(this.createTime.getTime() + 8 * 60 * 60 * 1000);
-        
+
         // Header
         fs.writeFile(this.absolutePath,
             "================================================================================\n" +
             "                       - Log file for GhostNight server -                       \n" +
             "================================================================================\n" +
             " - File name: " + options.fileName + "\n" +
-            " - Create time: " + this.createTime.toLocaleDateString() + " " + this.createTime.toLocaleTimeString() + "\n" + 
+            " - Create time: " + this.createTime.toLocaleDateString() + " " + this.createTime.toLocaleTimeString() + "\n" +
             " - Room Number: " + data.settings.Room + "\n" +
             // " - Setting: " + data.settings + "\n" +
             "================================================================================\n" +
             " Timestamp | Event source | Event name | Data                                   \n" +
             "--------------------------------------------------------------------------------"
-            , {encoding: 'utf8', mode: 0666, flag: "w"}, null);
+            , {encoding: 'utf8', mode: 0666, flag: "w"}, console.error);
     }
 }
 Logger.prototype.Log = function(timestamp, unit, event, data) {
@@ -32,11 +32,11 @@ Logger.prototype.Log = function(timestamp, unit, event, data) {
             + ("          " + event).substr(-10, 10) + " | "
             + data;
     if(this.absolutePath != undefined) {
-        fs.appendFile(this.absolutePath, str, {encoding: 'utf8'}, null);
+        fs.appendFile(this.absolutePath, str, {encoding: 'utf8'}, console.error);
     }
 }
 Logger.prototype.Statistics = function(data) {
-    var str = "\n" + 
+    var str = "\n" +
             "================================================================================\n" +
             "                                 - Statistics -                                 \n" +
             "================================================================================\n" +
@@ -44,8 +44,8 @@ Logger.prototype.Statistics = function(data) {
     for(var record in data) {
         str += "\n" + record.type + ": " + record.damage;
     }
-    
-    fs.appendFile(this.absolutePath, str, {encoding: 'utf8', mode: 0666, flag: "w"}, null);
+
+    fs.appendFile(this.absolutePath, str, {encoding: 'utf8', mode: 0666, flag: "w"}, console.error);
 }
 
 module.exports = Logger;
